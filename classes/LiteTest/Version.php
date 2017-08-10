6b91a6df9c844ae6b584b92a9ee6b790b1fd8d53
ROBERT <rob@whiteacorn.com>
2017-08-09 22:41:39 -0700

    trying to get rid of shebank echo

--- classes/LiteTest/Version.php
+++ classes/LiteTest/Version.php
@@ -2,5 +2,5 @@
 namespace LiteTest;
 class Version
 {
-	public static $version = "0.0.71";
+	public static $version = "0.0.72";
 }
--- classes/Main.php
+++ classes/Main.php
@@ -1,3 +1,2 @@
-#!/usr/bin/env php
 <?php
 require_once(__DIR__."/LiteTest/litetest.php");
